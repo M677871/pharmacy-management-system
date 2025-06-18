b0ac0126d1e59cfa2c1f30e627c59033ccc6ac15
M677871 <miledaz1234@gmail.com>
2025-06-18 14:19:34 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import test from 'node:test';
 require('dotenv').config();
 
 const port = process.env.PORT ;
@@ -18,3 +19,20 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// src/config/testConnection.ts
+
+import sequelize from '../config/db';
+
+const testConnection = async () => {
+  try {
+    console.log('Testing database connection...');
+    await sequelize.authenticate(); // actual connection test
+    console.log('✅ Database connection successful!');
+  } catch (error) {
+    console.error('❌ Database connection failed:', error);
+    process.exit(1); // exit the process if connection fails
+  }
+};
+
+testConnection();
