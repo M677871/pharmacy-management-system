61225bf0c1f14a5025702022699dc3638a28e0bd
M677871 <miledaz1234@gmail.com>
2025-06-18 14:04:44 +0300

    a pharmacy project using typescript mysql and orm in the backend and react in frontend

---
+++ config/db.ts
@@ -0,0 +1,18 @@
+import { Sequelize } from 'sequelize';
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME ,
+  process.env.DB_USER ,
+  process.env.DB_PASS ,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    dialectModule: db,
+    logging: false, // Disable logging; default: console.log
+  }
+);
+
+
+module.exports = sequelize;
+export default sequelize;
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+require('dotenv').config();
+
+const port = process.env.PORT ;
+const app = express();
+
+app.use(express.json());
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
