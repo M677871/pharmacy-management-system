e73e94cbde184e5779347af18fac4507f4973015
M677871 <miledaz1234@gmail.com>
2026-03-14 21:35:07 +0200

    adding the model entity of the category

---
+++ backend/src/features/inventory/categories/entities/category.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Product } from '../../products/entities/product.entity';
+
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', unique: true })
+  name: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  description: string | null;
+
+  @OneToMany(() => Product, (product) => product.category)
+  products: Product[];
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+}
