e7bd561d1c93ee7f401a0c67a8c2760ae3d2b819
M677871 <miledaz1234@gmail.com>
2026-04-02 22:29:47 +0300

    Register purchases module

---
+++ backend/src/features/inventory/purchases/purchases.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BatchesModule } from '../batches/batches.module';
+import { ProductsModule } from '../products/products.module';
+import { PurchaseItemsModule } from '../purchase-items/purchase-items.module';
+import { StockMovementsModule } from '../stock-movements/stock-movements.module';
+import { SuppliersModule } from '../suppliers/suppliers.module';
+import { PurchasesController } from './purchases.controller';
+import { Purchase } from './entities/purchase.entity';
+import { PurchasesRepository } from './purchases.repository';
+import { PurchasesService } from './purchases.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Purchase]),
+    SuppliersModule,
+    ProductsModule,
+    PurchaseItemsModule,
+    BatchesModule,
+    StockMovementsModule,
+  ],
+  controllers: [PurchasesController],
+  providers: [PurchasesRepository, PurchasesService],
+  exports: [PurchasesRepository, PurchasesService],
+})
+export class PurchasesModule {}
