59b806fab0e72a68fd2483225e76275a1f72bbbd
M677871 <miledaz1234@gmail.com>
2026-04-09 22:49:18 +0300

    feat(messaging): register MessagingModule in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,9 @@ import { DatabaseModule } from './database/database.config';
 import { AuthModule } from './features/auth/auth.module';
 import { DashboardModule } from './features/dashboard/dashboard.module';
 import { InventoryModule } from './features/inventory/inventory.module';
+import { RealtimeModule } from './features/realtime/realtime.module';
 import { UsersModule } from './features/users/users.module';
+import { MessagingModule } from './features/messaging/messaging.module';
 
 @Module({
   imports: [
@@ -13,6 +15,8 @@ import { UsersModule } from './features/users/users.module';
     AuthModule,
     DashboardModule,
     InventoryModule,
+    RealtimeModule,
+    MessagingModule,
     UsersModule,
   ],
 })
